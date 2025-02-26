f61e11bd38b5a05f4fbe0a73599fbfb1abf30c8a
Lucas Xavier do Nascimento Souza <143665539+Lucas-Br10@users.noreply.github.com>
2025-02-26 12:00:21 -0300

    Add files via upload

---
+++ tipos_de_dados.py
@@ -0,0 +1,12 @@
+
+print(1+11) # Tipo Inteiro
+print(111111 + 1.2) # Tipo Flutuante (Float)
+print(True) # Tipo Booleano
+print(False) # Tipo Booleano
+print("Python") # Tipo String 
+
+
+int() 
+float()
+str()
+bool()

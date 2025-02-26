dbfab5025c0027648247f919ef3b3bcd4c9bc650
Lucas Xavier do Nascimento Souza <143665539+Lucas-Br10@users.noreply.github.com>
2025-02-26 20:00:21 -0300

    Add files via upload

---
+++ print_input.py
@@ -0,0 +1,12 @@
+
+
+nome = input("Infprme o seu nome: ") 
+# Atribui a string que o usuário digitar ao nome
+idade = input ("Informe a sua idade: ")
+# Atribui a string que o usuário digitar a idade
+
+print(nome, idade)
+print(nome, idade, end="...\n")
+# end="...\n" é a quebra de linha
+print(nome, idade, sep ="#")
+# sep é de separador 

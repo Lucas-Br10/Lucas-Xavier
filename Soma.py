685313c8bb9a16de72b025186712632a43c72f55
Lucas Xavier do Nascimento Souza <143665539+Lucas-Br10@users.noreply.github.com>
2025-02-26 10:33:55 -0300

    Add files via upload

---
+++ Soma.py
@@ -0,0 +1,19 @@
+# Soma de Dois números
+
+print("Digite um número?")
+Numero1 = int(input())
+# Sempre colocar uma variavel (Int ou Float)
+
+
+print("Digite outro número?")
+Numero2 = int(input())
+# Sempre colocar uma variavel (Int ou Float)
+
+soma = Numero1 + Numero2
+
+print("A soma dos numeros é:", soma)
+
+""" 
+Se não for atribuido a varialvel ele séra considerada uma string
+Com isso não é feita a soma, e sim a junção dos número.
+""" 
